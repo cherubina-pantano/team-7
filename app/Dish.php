275c9c8d47423dead66a38dc89413b3c377d39bf
FrancescoAmato87 <francesco.duff@gmail.com>
2021-02-15 18:19:01 +0100

    Seeders Done

--- app/Dish.php
+++ app/Dish.php
@@ -9,4 +9,8 @@ class Dish extends Model
     public function restaurants() {
         return $this->belongsTo('App\Restaurant');
     }
+
+    public function orders() {
+        return $this->belongsToMany('App\Order');
+    }
 }
