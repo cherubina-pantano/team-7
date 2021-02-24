7f8b4fb85a72818fa19565cb4e21aa56c7621a53
Giulia <giulia.guerrazzi@live.it>
2021-02-24 10:26:06 +0100

    filter restaurants

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         Cerca per tipologia
 
-        <input type="text" name="name" id="name" v-model="name">
+        <input type="text" name="name" id="name" v-model="name" v-on:keyup="filterType">
 
     
         <div class='form-group'>
@@ -25,37 +25,10 @@
             </ul>
         </div>
 
-      
-
- 
-   
-        <!-- <select v-model="actualType"
-        v-on:change="filterType"
-        >
-            <option value="tutte">tutte</option>
-            @foreach ($types as $type)
-
-                <option value="{{$type->type}}">
-                    {{$type->type}}
-                </option>
-            @endforeach
-        </select>
-        
-        <ul v-if="types.length > 0">
-            <li v-for="type in types">
-                <a href="">
-                    <h2>@{{type.name}}</h2>
-                </a>
-                <p>@{{type.address}}</p>
-                <p>@{{type.type}}</p>
-                <p>@{{type.phone}}</p>
-            </li>
-        </ul>
-        <h2 v-else>Siamo spiacenti, non ci sono ancora ristoranti</h2> -->
     </div>
 
 
 @endsection
 
-<script src="{{asset('js/app.js')}}" defer></script>
+<script src="{{asset('js/app.js')}}"></script>
 
